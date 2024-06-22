dc6f453419b344f21e790724edac9dc60840fee8
Kalidass <kalidass.k021@gmail.com>
2024-06-22 23:03:56 +0530

    implement filter restaurant using use state

--- README.md
+++ README.md
@@ -42,3 +42,21 @@
  * - Address
  * - Contact
  */
+
+
+Two types of import/ export
+
+- Default export/ import
+    export default Component;
+    import Component from "path";
+- Named export/ import
+    export const Component;
+    import {Component} fromm "path";
+
+
+
+# React Hooks
+
+    - Normal JavaScript utility functions
+    - useState() - Super powerful state variables.
+    - useEffect() - 
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,21 +1,26 @@
-import RestaurantCard  from "./RestaurantCard";
+import { useState } from "react";
+import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
 
 const Body = () => {
-
-      
-    return (
-      <div className="body">
-        <div className="search">Search</div>
-        <div className="res-container">
-          {
-            resList.map((restaurant) => <RestaurantCard key={restaurant.info.id} resData={restaurant} />)
-          }
-  
-          
-        </div>
+  const [restaurantList, setRestaurantList] = useState(resList);
+  return (
+    <div className="body">
+      {/* <div className="search">Search</div> */}
+      <div className="filter">
+        <button className="filter-btn" onClick={() => {
+          let filteredList = restaurantList.filter(res => res.info.avgRating > 4);
+          setRestaurantList(filteredList);
+          console.log('filtered list', filteredList);
+        }}>Top rated Restaurants</button>
       </div>
-    );
-  };
+      <div className="res-container">
+        {restaurantList?.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-  export default Body;
+export default Body;
