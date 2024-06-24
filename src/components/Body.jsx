4d2107598506d36ff7ef967c335d5fe6fc21cece
Kalidass <kalidass.k021@gmail.com>
2024-06-24 20:04:07 +0530

    rewrite

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,16 +3,16 @@ import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
 
 const Body = () => {
+  const filterBtnClick = () => {
+    let filteredList = restaurantList.filter(res => res.info.avgRating > 4);
+    setRestaurantList(filteredList);
+  }
   const [restaurantList, setRestaurantList] = useState(resList);
   return (
     <div className="body">
       {/* <div className="search">Search</div> */}
       <div className="filter">
-        <button className="filter-btn" onClick={() => {
-          let filteredList = restaurantList.filter(res => res.info.avgRating > 4);
-          setRestaurantList(filteredList);
-          console.log('filtered list', filteredList);
-        }}>Top rated Restaurants</button>
+        <button className="filter-btn" onClick={filterBtnClick}>Top rated Restaurants</button>
       </div>
       <div className="res-container">
         {restaurantList?.map((restaurant) => (
