5579a9b7a212a99f74b79c1bb7fd309788f368d8
Kalidass <kalidass.k021@gmail.com>
2024-06-27 21:13:30 +0530

    implement shimmer loader

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
+// import resList from "../utils/mockData";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [restaurantList, setRestaurantList] = useState(resList);
+  const [restaurantList, setRestaurantList] = useState([]);
 
   const filterBtnClick = () => {
     let filteredList = restaurantList.filter(res => res.info.avgRating > 4);
@@ -11,8 +12,25 @@ const Body = () => {
   }
 
   useEffect(() => {
-    console.log('useEffect is called');
-  }, [])
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.89960&lng=80.22090&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
+      const json = await data.json();
+      console.log('json', json);
+      setRestaurantList(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    } catch (err) {
+      console.error(`Error while fetching the data ${err}`);
+      throw err;
+    }
+  }
+
+  // if (restaurantList.length === 0)
+  if (!restaurantList.length) {
+    return <Shimmer />
+  }
   return (
     <div className="body">
       {/* <div className="search">Search</div> */}
