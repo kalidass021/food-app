f06e7ec2e9e4a8da2a2168bb3a620a7fe262c790
Kalidass <kalidass.k021@gmail.com>
2024-06-27 22:31:03 +0530

    useState login/logout

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -7,9 +7,9 @@ const Body = () => {
   const [restaurantList, setRestaurantList] = useState([]);
 
   const filterBtnClick = () => {
-    let filteredList = restaurantList.filter(res => res.info.avgRating > 4);
+    let filteredList = restaurantList.filter((res) => res.info.avgRating > 4);
     setRestaurantList(filteredList);
-  }
+  };
 
   useEffect(() => {
     fetchData();
@@ -17,25 +17,32 @@ const Body = () => {
 
   const fetchData = async () => {
     try {
-      const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.89960&lng=80.22090&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
+      const data = await fetch(
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.89960&lng=80.22090&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      );
       const json = await data.json();
-      console.log('json', json);
-      setRestaurantList(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+      // optional chaining
+      setRestaurantList(
+        json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants
+      );
     } catch (err) {
       console.error(`Error while fetching the data ${err}`);
       throw err;
     }
-  }
+  };
 
+  // conditional rendering
   // if (restaurantList.length === 0)
-  if (!restaurantList.length) {
-    return <Shimmer />
-  }
-  return (
+  return !restaurantList.length ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       {/* <div className="search">Search</div> */}
       <div className="filter">
-        <button className="filter-btn" onClick={filterBtnClick}>Top rated Restaurants</button>
+        <button className="filter-btn" onClick={filterBtnClick}>
+          Top rated Restaurants
+        </button>
       </div>
       <div className="res-container">
         {restaurantList?.map((restaurant) => (
