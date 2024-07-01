5de5bf3fbfbfdc3ad769db7b92f0cbb5e153eb6e
Kalidass <kalidass.k021@gmail.com>
2024-07-01 22:00:25 +0530

    react router dom initial setup and config

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-
-
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+// routing
+// step1 - import createBrowserRouter
+// step3 - import RouterProvider
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
 // const styleCard = {
 //   backgroundColor: "#f0f0f0"
@@ -19,8 +24,27 @@ const AppLayout = () => {
   );
 };
 
+
+// step2 - Configuration
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    // not found page
+    errorElement: <Error />
+  },
+  {
+    path: "/about",
+    element: <About />
+  },
+  {
+    path: "/contact",
+    element: <Contact />
+  }
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // root.render(heading);
-
-root.render(<AppLayout />);
+// step4 - Providing App to the RouterProvider
+root.render(<RouterProvider router={appRouter}/>);
