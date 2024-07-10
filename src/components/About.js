b47f3efc0d2896bb74ad81abad5df99249863b68
Kalidass <kalidass.k021@gmail.com>
2024-07-10 22:44:01 +0530

    parent child class based component

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,36 @@
+import {Component} from "react";
 import User from "./User";
 import UserClass from "./UserClass";
 
-const About = () => {
-    return (
-        <div>
-            <h1>About page</h1>
+class About extends Component {
+    constructor(props) {
+        super(props);
+        console.log("Parent Constructor");
+    }
+
+    componentDidMount() {
+        console.log("Parent compoent did mount");
+    }
+
+    render() {
+        console.log("Parent Render");
+        return (
+            <div>
+            <h1>About Class Compoenent</h1>
             {/* <User name={"Kalidass (functional)"} /> */}
             <UserClass name={"Kalidass (class)"} location={"RMD, TN, India (class)"}/>
-        </div>
-    );
+        </div> 
+        );
+    }
 }
+// const About = () => {
+//     return (
+//         <div>
+//             <h1>About page</h1>
+//             {/* <User name={"Kalidass (functional)"} /> */}
+//             <UserClass name={"Kalidass (class)"} location={"RMD, TN, India (class)"}/>
+//         </div>
+//     );
+// }
 
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -5,10 +5,17 @@ class UserClass extends React.Component {
     super(props);
     this.state = {
       count: 0,
+      count2: 2,
     };
+    console.log("Child constructor");
+  }
+
+  componentDidMount() {
+    console.log("Child component did mount");
   }
 
   render() {
+    console.log("Child render");
     const { name, location } = this.props;
     const { count, count2 } = this.state;
     return (
