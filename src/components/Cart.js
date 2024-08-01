2c8ef865c7d1c5f1930260f23bc1ec0cc9ef6602
Kalidass <kalidass.k021@gmail.com>
2024-08-01 11:01:21 +0530

    implement clear cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,17 +1,35 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import ItemList from "./ItemList";
+import { clearCart } from "../redux/slices/cartSlice";
 
 const Cart = () => {
-    const cartItems = useSelector((store) => store.cart.items);
-    console.log('cartItems', cartItems);
-    return (
-        <div className="text-center m-4 p-4">
-            <h1 className="text-2xl font-bold">Cart</h1>
-            <div className="w-6/12 m-auto">
-                <ItemList itemCards={cartItems}/>
-            </div>
-        </div>
-    );
-}
+  const cartItems = useSelector((store) => store.cart.items);
+  const dispatch = useDispatch();
+  // function to handle the clear cart
+  const handleClearCart = () => {
+    // dispath an action
+    dispatch(clearCart());
+  };
+  return (
+    <div className="text-center m-4 p-4">
+      <h1 className="text-2xl font-bold">Cart</h1>
+      <div className="w-6/12 m-auto">
+        {/* cartItems.length === 0 */}
+        {!cartItems.length ? (
+          <h1>Cart is empty, Add items to the cart</h1>
+        ) : (
+          <button
+            className="p-2 m-2 bg-green-600 text-white rounded-lg"
+            onClick={handleClearCart}
+          >
+            Clear Cart
+          </button>
+        )}
+
+        <ItemList itemCards={cartItems} />
+      </div>
+    </div>
+  );
+};
 
 export default Cart;
