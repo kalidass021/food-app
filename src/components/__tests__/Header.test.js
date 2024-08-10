7bdadfd0e77b74815aebc4550d032071ed0e07f8
Kalidass <kalidass.k021@gmail.com>
2024-08-10 13:26:52 +0530

    Test cases for header component

--- src/components/__tests__/Header.test.js
+++ src/components/__tests__/Header.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Header from "../Header";
 import { Provider } from "react-redux";
 import appStore from "../../redux/appStore";
@@ -23,4 +23,68 @@ it("Should render header component with login button", () => {
   // assertion
   expect(loginButton).toBeInTheDocument();
 });
+
+
+// test case 2
+it("Shold render header component with Cart items 0", () => {
+  // render the Header component to the JSDOM
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  // querying
+  // const cartItems = screen.getByText("Cart: 0 items");
+  // using regex
+  const cartItems = screen.getByText(/Cart/);
+
+  // assertion
+  expect(cartItems).toBeInTheDocument();
+});
+
+
+// test case 3
+it("Shold render header component with Cart", () => {
+  // render the Header component to the JSDOM
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  // querying
+  // using regex
+  const cartItems = screen.getByText(/Cart/);
+
+  // assertion
+  expect(cartItems).toBeInTheDocument();
+});
+
+
+// test case 4
+it("Should change Login Button to Logout on click", ()=> {
+  // render the Header component to the JSDOM
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+  // querying
+  const loginButton = screen.getByRole("button", {name: "Login"});
+  // fire event
+   fireEvent.click(loginButton);
+   const logoutButton = screen.getByRole("button", {name: "Logout"});
+
+  // assertion
+  expect(logoutButton).toBeInTheDocument();
+});
+
+
 
