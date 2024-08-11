223d91db5543ce578167280e0c36bb2c664ee6d1
Kalidass <kalidass.k021@gmail.com>
2024-08-11 22:37:07 +0530

    test case for speedy higher order component

--- src/components/__tests__/RestaurantCard.test.js
+++ src/components/__tests__/RestaurantCard.test.js
@@ -1,11 +1,11 @@
 import { screen, render } from "@testing-library/react";
-import RestaurantCard from "../RestaurantCard";
+import RestaurantCard, {withSpeedyLabel} from "../RestaurantCard";
 import MOCK_DATA from "../mocks/resCardMock.json";
 import "@testing-library/jest-dom";
 
 // test case1
 it("Should render RestaurantCard component with props data", () => {
-    // render the resData component to the JSDOM with props
+    // render the RestaurantCard component to the JSDOM with props
     render(<RestaurantCard resData={MOCK_DATA} />);
 
     // querying
@@ -13,5 +13,20 @@ it("Should render RestaurantCard component with props data", () => {
 
     // assertion
     expect(name).toBeInTheDocument();
+
+});
+
+// test case2
+it("Should render RestaurantCard with speedy label", () => {
+    // test Higher Order Component with Speedy label
+    const RestaurantCardSpeedy = withSpeedyLabel(RestaurantCard);
+    // render the component to the JSDOM
+    render(<RestaurantCardSpeedy resData={MOCK_DATA}/>);
+
+    // querying
+    const withSpeedy = screen.getByText("Speedy");
+
+    // assertion
+    expect(withSpeedy).toBeInTheDocument();
 
 });
